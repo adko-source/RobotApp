ebfb692e34e16adf4057a409819bcff9175ed9fd
Adam Marko <adko87@hotmail.com>
2025-11-05 19:04:07 -0700

    update

--- RobotApp.Library/Robot.cs
+++ RobotApp.Library/Robot.cs
@@ -14,9 +14,10 @@ namespace RobotApp.Library
         public Direction CurrentDirection { get; set; }
         public List<char> Commands { get; set; } = new List<char>();
 
-        public Position? EndPosition { get; set; }
+        public Position? ExpectedEndPosition { get; set; }
 
-        public Direction EndDirection { get; set; }
+        public Direction ExpectedEndDirection { get; set; }
+        
 
 
         public List<char> GiveInstructionsAndStart(string[] instuctions, char[,] grid)
@@ -29,19 +30,13 @@ namespace RobotApp.Library
             {
                 StartPosition = journey.GetStartPosition();
 
-                System.Console.WriteLine($"journey start pos {string.Join(" ", StartPosition)}");
-
                 StartDirection = journey.GetStartDirection();
-
-                System.Console.WriteLine($"journey start dir {StartDirection}");
 
                 Commands = journey.GetCommands();
 
-                System.Console.WriteLine($"journey commands {string.Join("", Commands)}");
+                ExpectedEndPosition = journey.GetEndPosition();
 
-                EndPosition = journey.GetEndPosition();
-
-                EndDirection = journey.GetEndDirection();
+                ExpectedEndDirection = journey.GetEndDirection();
 
                 Start(grid);
             };
@@ -51,6 +46,13 @@ namespace RobotApp.Library
 
         private void Start(char[,] grid)
         {
+            System.Console.WriteLine($"journey started pos {string.Join(" ", StartPosition)}");
+            System.Console.WriteLine($"journey started dir {StartDirection}");
+            System.Console.WriteLine($"journey commands {string.Join("", Commands)}");
+
+            System.Console.WriteLine($"journey expected end pos {string.Join(" ", ExpectedEndPosition)}");
+            System.Console.WriteLine($"journey expected end dir {ExpectedEndDirection}");
+
             CurrentPosition = StartPosition;
 
             CurrentDirection = StartDirection;
@@ -70,16 +72,21 @@ namespace RobotApp.Library
                         Console.WriteLine("OUT OF BOUNDS");
                         break;
                     }
-
-                    if (IsCrashed(grid))
+                    else if (IsCrashed(grid))
                     {
                         Console.WriteLine($"CRASHED {CurrentPosition.Col} {CurrentPosition.Row} {CurrentDirection}");
                         break;
                     }
-                    
-                    
                 }
             }
+
+            var journeyResult = $"{CurrentPosition.Col} {CurrentPosition.Row} {CurrentDirection}";
+            
+            var expectedResult = $"{ExpectedEndPosition.Col} {ExpectedEndPosition.Row} {ExpectedEndDirection}";
+
+            var status = journeyResult == expectedResult ? "SUCCESS" : "FAILURE";
+
+            Console.WriteLine($"{status} {journeyResult}");
         }
 
         public void Turn(char command)
@@ -110,30 +117,34 @@ namespace RobotApp.Library
 
         private void Move()
         {
-            var newRow = CurrentDirection switch
+            var rowOffset = CurrentDirection switch
             {
-                Direction.N => +1,
+                Direction.N => 1,
                 Direction.S => -1,
-                _ => CurrentPosition.Row
+                _ => 0
             };
 
-            int newCol = CurrentDirection switch
+            var colOffset = CurrentDirection switch
             {
-                Direction.E => +1,
+                Direction.E => 1,
                 Direction.W => -1,
-                _ => CurrentPosition.Col
+                _ => 0
             };
 
-            var newPosition = CurrentPosition with { Col = CurrentPosition.Col + newCol, Row = CurrentPosition.Row + newRow };
+            var newPosition = CurrentPosition with
+            {
+                Row = CurrentPosition.Row + rowOffset,
+                Col = CurrentPosition.Col + colOffset
+            };
 
             CurrentPosition = newPosition;
         }
 
         private bool IsCurrentPositionValid(char[,] grid)
         {
-            var maxRows = grid.GetLength(0);
+            var maxRows = grid.GetLength(0) -1;
 
-            var maxCols = grid.GetLength(1);
+            var maxCols = grid.GetLength(1) -1;
 
             if (CurrentPosition?.Row > maxRows || CurrentPosition?.Row < 0 || CurrentPosition?.Col >= maxCols || CurrentPosition?.Col < 0)
             {
