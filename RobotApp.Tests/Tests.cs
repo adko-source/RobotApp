ca4065e66bafc7ecb272c1af8f45c9d229d91cb7
Adam Marko <adko87@hotmail.com>
2025-11-06 18:51:36 -0700

    update

--- RobotApp.Tests/Tests.cs
+++ RobotApp.Tests/Tests.cs
@@ -6,7 +6,7 @@ namespace RobotApp.Tests
     public class RobotTests
     {
         [Fact]
-        public void MoveRobot_North_IncrementsRow()
+        public void MoveRobot_North_AddsRow()
         {
             var robot = new Robot
             {
@@ -19,6 +19,41 @@ namespace RobotApp.Tests
             // Assert
             Assert.Equal(2, robot.CurrentPosition.Row);
             Assert.Equal(1, robot.CurrentPosition.Col);
+        }
+
+        [Fact]
+
+        public void MoveRobot_South_SubtractsRow()
+        {
+            var robot = new Robot
+            {
+                CurrentPosition = new Position(1, 3),
+                CurrentDirection = Direction.S
+            };
+
+            robot.Move();
+
+            // Assert
+            Assert.Equal(2, robot.CurrentPosition.Row);
+            Assert.Equal(1, robot.CurrentPosition.Col);
+        }
+
+        [Fact]
+
+        public void MoveRobot_East_AddsColumn()
+        {
+            var robot = new Robot
+            {
+                CurrentPosition = new Position(0, 3),
+                CurrentDirection = Direction.E
+            };
+
+            robot.Move();
+            robot.Move();
+
+            // Assert
+            Assert.Equal(3, robot.CurrentPosition.Row);
+            Assert.Equal(2, robot.CurrentPosition.Col);
         }
 
         [Fact]
@@ -35,17 +70,48 @@ namespace RobotApp.Tests
         }
 
         [Fact]
-        public void StartJourney_OutOfBounds_ReturnsOutOfBounds()
+        public void TurnRobot_Left_FromNorth_UpdatesDirection()
         {
-            // Arrange
-            var grid = new char[3,3];
-            var robot = new Robot();
+            var robot = new Robot
+            {
+                CurrentDirection = Direction.N
+            };
 
-            // Act
-            robot.RunCommands(grid);
+            robot.Turn(Command.L);
 
-            // Assert
-            //Assert.Contains("OUT OF BOUNDS", result);
+            Assert.Equal(Direction.W, robot.CurrentDirection);
+        }
+
+        [Fact]
+        public void StartJourney_OutOfBounds_ReturnsTrue()
+        {
+            var grid = new char[3, 3];
+
+            var robot = new Robot
+            {
+                CurrentPosition = new Position(4, 3),
+            };
+
+            var result = robot.IsOutOfBounds(grid);
+
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void StartJourney_IsCrashed_ReturnsTrue()
+        {
+            var grid = new char[3, 3];
+
+            grid[2, 2] = 'O';
+            
+            var robot = new Robot
+            {
+                CurrentPosition = new Position(2, 2),
+            };
+
+            var result = robot.IsCrashed(grid);
+
+            Assert.True(result);
         }
     }
 }
